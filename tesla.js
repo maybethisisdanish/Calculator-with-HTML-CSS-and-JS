ed97302c92ff34bc593d12055faf5f57e84c27f6
maybethisisdanish <134921315+maybethisisdanish@users.noreply.github.com>
2023-06-03 21:47:11 +0530

    Update tesla.js

--- tesla.js
+++ tesla.js
@@ -9,28 +9,28 @@
                 var maxrange = document.getElementById("maxrange");
 
                 ms.onclick = function(){
-                    header.style.backgroundImage = "url(images/image-1.png)";
+                    header.style.backgroundImage = "url(https://lh3.googleusercontent.com/Vd1L7JBhs-vPtIlZpOCdkd0JnRWg75D2ffdJGmaMRbt9W9j9_1NEXgYO4CCe-husHH1KnTShINE0kdUlqYVp02i11ZoPo6lPdJyJh1ZfyPWql2V-zVA28FR0kH-CLzUy4qd6s005zADDxjFqrGu7YpwEXhjuSHQY73NaWiMOx26iyHYDJV6shGhdfKZ0F4ZzintRW0ES9CS89SjzJztEK_AwD00MGcCdnsj0m-cip_u4FVcHuB5bCf0DrJ_yP4o2Mp6kEUV7s1qxocFO2lgk00oYjvkFUCLSWB5s4XD16SiJ6sTSpfCsAuQOLFvrg1LRQck-lTbhWOs86M6Kb7cec8eIcYaeXoGTHWlSSNv6iqXXiwHmWVbDtATDYgDx9HYzQFcFnJSWrsvcU76xJ0ikbliS_bDIpuVhBiIe_6BYkmDAqubueGKVmJkDX8Tl_WSC9CzfXgq57ZazuOD7sWeTM-eNABNkyPj38VRrH11cEgOOCPTl3dAzWjWc4xt560V6EyEBrgsofWP4dGSYm7F0l8Z0WjpFpbdpD_kTcVmzQdupl3jUFYlwx17IiL2wazA1SF2rGFzuy8ld04wCdNSWPK9NceySMepnnnjjapGe709AzXPDGO6uJGOL3Gg9NLf_8lt02Rem_jvnAxJyRzCPD2cUiNaZI2GQvubB_rkdXRT-uRooB-w5fb1XVb6IwXUmzEDVTXtUkt5DuS8x5xVfhYdMpccswRJyNugvV5xUxON0A9ENO9qCB8PyvVwRxIbdIO1ifSiU9VDPEJWDjXUXBJSfRtHYvBKgYCzRChSiNucoiCXzzfJOEo14tt2xBF7xzYoRqOUQXKYdAOomMWvptw84_CiDPXfeAzfWS1uvZaTp3_MoZPJkz6VO2C_RQ2w51am128cNWSigc10oGlAx1vLxIH3PbTgX2HkaHd5kONhAGFZiCA=w1111-h625-s-no?authuser=0)";
                     model.innerHTML = "Model S";
                     zerotosixty.innerHTML = "1.9s";
                     topspeed.innerHTML = "200mph";
                     maxrange.innerHTML = "396mi";
                 }
                 m3.onclick = function(){
-                    header.style.backgroundImage = "url(images/image-2.png)";
+                    header.style.backgroundImage = "url(https://lh3.googleusercontent.com/_gQdIZLUbpzpjtNsZZbiynYgTv6EaQrA777kI9rByqGzi98qN3WcA0TQC66i6xdS-WliQjvZcXTTHkRGIJQOn9DgQBEIKvb3kgEsmkpjvS1qlr6xO5MPzlG6Z5kSWnZWle86CFivE77uj3PDerKKu6i4faWx6cpxO_3N56jge-4d1zDFeliB83ZA96YCkrQt8ACUwZwTL3og_N4kdG2QoHqXPC_vgKutZuKkLnXKHlaDkG1bLqYJUIlzD88TCfZBMRBLncXael3_Da3s7eaBUuzWUD1dzjXGmx-BqoLmpGsCLKeDEGKLOzt15n4yn6q6DuviOlIvngAj6y67cHPhNS_pZw_314wPIvo9kqrh_JLhbYMiiqJLye260ncOHA20tuZRqlBioZvzaZJQkSz4rkmHr-EtLWHB58W7yCNXb6AlSVYsYw1XTtbY_MwaAw17nroZhuk_iq6AorqnP_PZjXD-MgrIupU0oOYKOZHkGIH0fiQB5Z7trf90xNrL5bfLg1chOoxUz6c0Hps7Yzfy23zeQcMXkHvUUprAhvaiPMIibsIaL5ZT_xDZuZX4i60qYvCha1AqEZokNZ56La_DO_DOgBGbqNSMwBlFJgkNQVRYRCRa1Ed3S7YSPsyFy_mcdkoHC0iXJeKWabo0lqLEFBd8mJkEdjuaBeR3u0EgmngX-3irMHxFurUSJD-HWmDXVWZkB8eIhcCm0uW8IJ7umgRE-UfhlbblhMs1BUQg-2t2VSqxklI8_3-ZEwRQ2LCbutXDwM5kzp2ZT92j0wh8dUqrqLNjqp8Kw_pxj3nSJTQgmpum5jro-Qhik8yPaSF1KLIUUwj3_FYQhLH0r18L_27jwUXYXaKP6gndcwifFhJM6ssvy0n9zBEBMiYsrCwy_k1JxQlA6FLkEFCAG2ufz5vFmXLOiLsXkPFP-8RhvxMV_YoXwA=w1111-h625-s-no?authuser=0)";
                     model.innerHTML = "Model 3";
                     zerotosixty.innerHTML = "1.7s";
                     topspeed.innerHTML = "220mph";
                     maxrange.innerHTML = "415mi";
                 }
                 mx.onclick = function(){
-                    header.style.backgroundImage = "url(images/image-3.png)";
+                    header.style.backgroundImage = "url(https://lh3.googleusercontent.com/Qc4qvy6d9lE7N_NJXheU-t7Z-Xlo_gXXF1xNoHVl6H1UFBV8tc3K3FFmq31U7oWDkvBHBgYs06Kt-L47tBQL0_H58bHo2bFyYTmeWY1BriI5zRaT25PcJ4PKmoUrw7ZIVSaNL2o38ObNsvQxrqUQ3ftD5ZDM_PEAaefW8RSIZETyicZVKpxQHjSqCIyVkAxfLpPVtT7S9ChSzVQ3m73IT-sdJdOobor4oVgbgplY56vd40bywXzSIsEC89uK_Y-g7EzKROogN8KH2AiiyRr2O9LN_dWy4uTDry_o4Bg7ZnAjvkhxk3Ds_I90it-InPZdLB3burVxihyCMuV1ovHENCa5DNa94K0YKtWwwO4bVv7StJHchskbeNEUcuQFJ_3GbJPdJyWIQ1vixFb8pevMwcbdcHPKevaOIbu5KwSo9WA-j9tDwp_MuZFZcjX18wZ2qwLBKZK2tvCTBa97GP0kE84WmeFJ62eFCbYhIfX6oap57Bs9YatutymITgstNoiFOtAT4LnM1fEgVQ9KloFPdokfuMDK_oMjCi3Uf8XA5r8mD2ZJeuYuagGQIZmZLsWz8Htbvt61FPZipYp99-0pqDKqYnr2WU2RIUkBs6Qk4PkJvTY8-5beAzTHXgBhoJSS-nFT0KwlJQFe9C-_0BdxJWdK-CAB-z6LxtZw2C-dFyz7jJ2MDf0n-dKak6rGQO3qZQnCToOVybAzc9PSfkshqoYjttZAWax_VSTIhukPsRVnICvzcc2AyRbJDaymK2mawH-YouTaTb8125u0u3tteEojW2LBjDGLhnchB9-kJFRH18UzRUc8kz3RNCyv0r6ReheQq0-fDl1BPkm-nsoiPzv2gm1QscCG3CjDKU1RyEB0mspkWV_9BFIIQYf8lQUuKLAQadkGC8ItJaRB9Vzq0W4YbHpBhQEL8Jl7w-rZ7-_jACJAAA=w1111-h625-s-no?authuser=0)";
                     model.innerHTML = "Model X";
                     zerotosixty.innerHTML = "1.5s";
                     topspeed.innerHTML = "240mph";
                     maxrange.innerHTML = "458mi";
                 }
                 my.onclick = function(){
-                    header.style.backgroundImage = "url(images/image-4.png)";
+                    header.style.backgroundImage = "url(https://lh3.googleusercontent.com/DGKDHMI4gcq-1sIeLs-SPZ6rv0T3p78vUpP5Nx51jFxSAvlwbXMSr8Dhrzfw3PFQSoNSdF_u0haYaGSjV7WP0tF0iGUF8RCVuK9w3pXCLK8o4Bho098x6ClWbFDFKw5ksMcpmDghOyq7Lr7Y7TJazibHvBQWfFs1r1lYDzc4XrX5DVghMGpog6HZGZUzKQfQrwaQMwavvgHFHg-Lzeji8P7XJcQuCHFZWHtyoz_ZvaBV7fXT0xhVR06J4RwYqWxN-NqYOe8TFY9QoFRZ3I3OQfwXqjzhV7EoLEhKHY0aXWSP2QCaj_o9mfmiHC2eqIVsRnXYf9e6XPfq4uBamnIQeUlHC90jp3hZxdG5DZ_ogl2qB8_Go4EQ7H1QFFCdGRQUb-aKamyU2JV3aE9RJ_RXZp_1J7KDu72c8bL4Ezf_w3nLKY9JpZeQUCpU-41qAGpjyX0PkiTwB-wuZ5ovDdYVA1IRJ40ubnRLMwZSsvGp3rem478o8RrQ37iNPgXBQvkYW5Q9eI3GibIASBgtae48NI-6JklfZQFBoRhljyU0mNcoXSlqmksK5spJZs6-nsF9TbjXOovXjI9sVAhT-7jm6v0N9EZRvHOrwtV_Y-VRtUg0Ulh3WNMTmotS_papk2nHlV0sSb-d91zXeoedZ8zkal0kHOqM_ZdK5xyFM48pinnAB19QYXUhYE_rROGe1lK2MINs48S9y-Y7Gok48AWtc_A5KT_1ZWF_O-KrntGdxaF4qSTNiBWzq7UgAcNBQygooGfAVp_EWArj78GE7hxXnaHh7qPyyBGbYXGV9BNamExAyQKLCj_MlcsGDNbhBvbf5_NLdRPVg3T1BjIfjddIa6toJJRJBsE8WZZ8nBayOQP8M2HcHQ3f7GLGDZlVIWrAkGZXz96utrJwfBitO9WdZe21lgJC0N9j07n-yCXoFq0Ad8LIeQ=w1111-h625-s-no?authuser=0)";
                     model.innerHTML = "Model Y";
                     zerotosixty.innerHTML = "1.3s";
                     topspeed.innerHTML = "250mph";
